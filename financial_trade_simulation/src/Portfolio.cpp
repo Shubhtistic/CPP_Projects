78bfccfd89737990e980879f7a0c1b7bf0e33dce
Shubham <theshubham1412@gmail.com>
2025-09-23 18:18:26 +0530

    completed implementation of Portfolio.cpp

--- financial_trade_simulation/src/Portfolio.cpp
+++ financial_trade_simulation/src/Portfolio.cpp
@@ -1,5 +1,6 @@
 #include "Portfolio.h"
 #include <iostream>
+#include <iomanip>
 
 bool Portfolio::buy(Asset *asset, int quantity)
 {
@@ -32,14 +33,45 @@ bool Portfolio::sell(Asset *asset, int quantity)
         return false;
     }
     double total_value_to_add = asset->get_value() * quantity;
-    total_cash = total_cash _ + total_value_to_add;
+    total_cash = total_cash + total_value_to_add;
     holdings[asset] = holdings[asset] - quantity;
 
-    if (holdings.count(asset) == 0)
+    if (holdings[asset] == 0)
     {
         holdings.erase(asset);
     }
 
     std::cout << "Success!!!\n";
     return true;
+}
+
+void Portfolio::pass_time()
+{
+    double total_payouts = 0.0;
+    for (auto const &holding_copy : holdings)
+    {
+        double payout = holding_copy.first->tick();
+        total_payouts = total_payouts + (payout * holding_copy.second);
+    }
+
+    total_cash = total_cash + total_payouts;
+}
+
+void Portfolio::display() const
+{
+
+    std::cout << "THe Current Total Balance is "
+              << total_cash << std::fixed << std::setprecision(3) << std::endl;
+
+    std::cout << "============= HOLDINGS!! ================\n";
+    if (holdings.empty())
+    {
+        std::cerr << "You have currently have no Holdings!!\n";
+    }
+    for (auto const &holdings1 : holdings)
+    {
+        std::cout << "Asset Symbol " << holdings1.first->get_symbol() << "\n";
+        std::cout << "Quantity owned " << holdings1.second << "\n";
+        std::cout << "Total Current Market value of holding " << holdings1.second * holdings1.first->get_value() << "\n";
+    }
 }
