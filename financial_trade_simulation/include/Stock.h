bfa37c865de6b4cd277df446618047a54dd62ca6
Shubham <theshubham1412@gmail.com>
2025-09-25 18:10:47 +0530

    Started to Work on csv file to store market assets this csv file will contain all the market assets and populates our vector with assets at the start of the program

--- financial_trade_simulation/include/Stock.h
+++ financial_trade_simulation/include/Stock.h
@@ -4,7 +4,7 @@
 class Stock : public Asset
 {
 public:
-    Stock(const std::string &symbol1, double price);
+    Stock(const std::string &symbol_given, double price_given);
     virtual double tick() override;
     virtual double get_value() const override;
 };
