6592d196c05890d26c97ac10f0c6bce998eb18f0
Shubham <theshubham1412@gmail.com>
2025-10-10 16:20:21 +0530

    log data type class

---
+++ log_file_analyzer/include/log.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+class log
+{
+    std::string time_stamp;
+    std::string level;
+    std::string info;
+
+public:
+    log(std::string time_stamp_given, std::string level_given, std::string info_given);
+    void log_parser(std::string log_line);
+};
