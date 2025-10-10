1aae436e14e8ec0420266c6618b858037fc17c01
Shubham <theshubham1412@gmail.com>
2025-10-10 16:26:58 +0530

    final log.h design

--- log_file_analyzer/include/log.h
+++ log_file_analyzer/include/log.h
@@ -7,5 +7,6 @@ class log
     std::string info;
 
 public:
-    log(std::string time_stamp_given, std::string level_given, std::string info_given);
+log::log(std::string time_stamp_given, std::string level_given, std::string info_given) : time_stamp(time_stamp_given), level(level_given), info(info_given) {}
+
 };
