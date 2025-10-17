d49547c6657ef1bbc53550a792e5e1b4ca1c21ba
Shubham <theshubham1412@gmail.com>
2025-10-17 16:51:26 +0530

    Add: new field for ip address in log struct

--- log_query_engine/include/log.h
+++ log_query_engine/include/log.h
@@ -8,6 +8,7 @@ struct log
     std::string timestamp;
     std::string log_level;
     std::string message;
+    std::string ip_address;
 
-    log(const std::string &t, const std::string &level,const std::string &msg) : timestamp(t), log_level(level), message(msg) {}
+    log(const std::string &t, const std::string &level, const std::string &msg, const std::string &ip) : timestamp(t), log_level(level), message(msg), ip_address(ip) {}
 };
