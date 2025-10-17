86bbae63977be7a5de9d3764fff73599026bd3a8
Shubham <theshubham1412@gmail.com>
2025-10-17 17:10:24 +0530

    Change: updated the parse_line method to also incorporate optional ip address

--- log_query_engine/src/simple_log_parser.cpp
+++ log_query_engine/src/simple_log_parser.cpp
@@ -10,9 +10,10 @@ std::optional<log> simple_log_parser::parse_line(const std::string &line)
         // if we are inside this block it means line was valid log entry
 
         // match_results will contain the full match at index 0,
-        // Our regex has 3 groups.
+        // Our regex has 4 groups.
+        // 3+1 -> 1 optional for ip address
         // a successful match will have a size of 4.
-        if (matches.size() == 4)
+        if (matches.size() == 5)
         {
             // matches[i] is like an pointer kind of like viweing so its not an string
             // with .str() we convert ino an string
@@ -20,11 +21,18 @@ std::optional<log> simple_log_parser::parse_line(const std::string &line)
             std::string timestamp = matches[1].str();
             std::string level = matches[2].str();
             std::string message = matches[3].str();
+            std::string ip_found = "";
+            if (matches[4].matched)
+            {
+                // as ip is optional i.e if exits the match block contains it
+                // else the match block is empty but still created
+                // If it matched, we extract the IP string.
+                ip_found = matches[4].str();
+            }
 
-            return log(timestamp, level, message);
+            return log(timestamp, level, message, ip_found);
         }
     }
     return std::nullopt;
     // we return an enpty signal, saying that parsing failed
 }
-
