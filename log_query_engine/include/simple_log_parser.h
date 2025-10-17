e693d226699dc5a7fcb1453f614dc6ff68d41c68
Shubham <theshubham1412@gmail.com>
2025-10-17 16:52:19 +0530

    Update: New Regex Pattern to process to optional ip address field

--- log_query_engine/include/simple_log_parser.h
+++ log_query_engine/include/simple_log_parser.h
@@ -7,6 +7,6 @@ class simple_log_parser : public log_parser
     std::regex simple_pattern;
 
 public:
-    simple_log_parser():simple_pattern(R"(^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{1,3})?)\]\s*(INFO|WARN|ERROR|DEBUG|FATAL)\s*-\s*(.*)$)"){}
+    simple_log_parser():simple_pattern(R"(^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}(?:\.\d{1,3})?)\]\s*(INFO|WARN|ERROR|DEBUG|FATAL)\s*-\s*(?:(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s*-\s*)?(.*)$)"){}
     std::optional<log> parse_line(const std::string &parsed) override;
 };
