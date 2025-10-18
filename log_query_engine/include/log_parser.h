3b1cf21d73b978453cc582e18ea7e487fc6c74e7
Shubham <theshubham1412@gmail.com>
2025-10-19 00:46:06 +0530

    Update: log_parser.h abstract class now has method return_parser_name to show its name
    Update: log_parser.h abstract class now has parser_name member variable and virtual return_parser_name member function
    
    Update: log_parser.h abstract class now has parser_name member variable and virtual return_parser_name member function

--- log_query_engine/include/log_parser.h
+++ log_query_engine/include/log_parser.h
@@ -1,11 +1,15 @@
 #pragma once
-#include<string>
-#include<optional>
+#include <string>
+#include <optional>
 #include "log.h"
-class log_parser{
-    public:
-    virtual std::optional<log> parse_line(const std::string& parsed)=0;
+class log_parser
+{
+    std::string parser_name;
 
-    virtual ~log_parser()=default;
+public:
+    virtual std::optional<log> parse_line(const std::string &parsed) = 0;
+    virtual std::string return_parser_name() const = 0;
+    // each class will provide their own implemenatation of this
 
+    virtual ~log_parser() = default;
 };
