68dae50417c92052a637ac17c32765d2f053672e
Shubham <theshubham1412@gmail.com>
2025-10-18 16:09:27 +0530

    Add: new member variable in log.h to hold the time parsed value

--- log_query_engine/include/log.h
+++ log_query_engine/include/log.h
@@ -1,6 +1,7 @@
 // this class has an simple struct which is our log object
 #pragma once
 #include <string>
+#include <chrono>
 
 struct log
 {
@@ -10,5 +11,12 @@ struct log
     std::string message;
     std::string ip_address;
 
-    log(const std::string &t, const std::string &level, const std::string &msg, const std::string &ip) : timestamp(t), log_level(level), message(msg), ip_address(ip) {}
+    // new member to store the parsed time object
+    std::chrono::system_clock::time_point timestamp_obj;
+    log(const std::string &ts, const std::string &lvl, const std::string &ip, const std::string &msg,
+        const std::chrono::system_clock::time_point &ts_obj) // <-- Add new parameter
+        : timestamp(ts), log_level(lvl), ip_address(ip), message(msg),
+          timestamp_obj(ts_obj)
+    {
+    }
 };
