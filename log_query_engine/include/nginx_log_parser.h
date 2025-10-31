38e65b53e26176af2270b43768bdc7039a9e1554
Shubham <theshubham1412@gmail.com>
2025-11-01 02:08:52 +0530

    Feat: Implementation of new parser :- nginx

---
+++ log_query_engine/include/nginx_log_parser.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "log_parser.h"
+#include <regex>
+#include <string>
+
+class nginx_log_parser : public log_parser
+{
+public:
+    nginx_log_parser();
+
+    std::optional<log> parse_line(const std::string &line) override;
+    std::string return_parser_name() const override;
+
+private:
+    std::regex nginx_pattern_;
+
+    std::optional<std::chrono::system_clock::time_point> parse_nginx_timestamp(const std::string &timestamp_str);
+    const std::string parser_name="NGINX Log Parser";
+};
