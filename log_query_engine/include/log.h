7a73c74908eb517c024fe4fac0be86c4f19cafe4
Shubham <theshubham1412@gmail.com>
2025-11-01 00:56:05 +0530

    Update: Added new apache log members in log.h
    Update: Added new apache log members in log.h
    
    Update: Added new Apache log members in log.h

--- log_query_engine/include/log.h
+++ log_query_engine/include/log.h
@@ -11,12 +11,22 @@ struct log
     std::string message;
     std::string ip_address;
 
+    // Apache Log members
+    std::string request;
+    int status_code;
+    size_t bytes_sent;
+
     // new member to store the parsed time object
     std::chrono::system_clock::time_point timestamp_obj;
+
     log(const std::string &ts, const std::string &lvl, const std::string &ip, const std::string &msg,
-        const std::chrono::system_clock::time_point &ts_obj) // <-- Add new parameter
+        const std::chrono::system_clock::time_point &ts_obj,
+        const std::string &req = "",
+        int status = 0,
+        size_t bytes = 0)
         : timestamp(ts), log_level(lvl), ip_address(ip), message(msg),
-          timestamp_obj(ts_obj)
+          timestamp_obj(ts_obj),
+          request(req), status_code(status), bytes_sent(bytes)
     {
     }
 };
